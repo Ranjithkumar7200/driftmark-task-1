3c3a420d9f684f52a43b333e0d4c7f4981f9866c
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2023-10-10 11:28:01 +0530

    Adding css file

--- src/Components/pages/Sidebar.js
+++ src/Components/pages/Sidebar.js
@@ -1,7 +1,9 @@
+import { hover } from '@testing-library/user-event/dist/hover';
 import React from 'react'
 import { Accordion, Col, Container, Row } from 'react-bootstrap'
 import { AiOutlineDashboard } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
+import '../pages/Css/Sidebar.css'
 
 const Sidebar = () => {
     const navigate = useNavigate();
@@ -21,9 +23,9 @@ const Sidebar = () => {
         
       };
   return (
-    <div className='rounded overflowX-scroll' style={{backgroundColor:"#00d4ff"}}>
+    <div className='rounded overflowX-scroll' style={{backgroundColor:"#00d4ff",}}>
     <Container fluid className='d-sm-none d-md-none d-lg-flex d-xxl-flex d-xl-flex flex-column justify-content-center align-items-start d-none mt-3'>
-        <Row className='mt-3 mb-3' style={{cursor:"pointer"}}>
+        <Row className='mt-3 mb-3 side' style={{cursor:"pointer"}}>
         <h6 onClick={handleAdminDashboardClick}>Admin Dashboard</h6>
         </Row>
         <Row className='mt-3 mb-3' style={{cursor:"pointer"}}>
