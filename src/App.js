3373d50fdc0d01e3062d66b4fc08c55425d9224b
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2023-10-10 17:59:09 +0530

    Using Authentication

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/pages/Login";
 import Register from "./Components/pages/Register";
-import Forgot from "./Components/pages/Forgot";
-import Reset from "./Components/pages/Reset";
 import MainNav from "./Components/pages/MainNav";
 import AdminDashboard from "./Components/pages/AdminDashboard";
-import EmployeeDashboard from "./Components/pages/EmployeeDashboard";
 import Designation from "./Components/pages/Designation";
+import EmployeeDashboard from "./Components/pages/EmployeeDashboard";
+import Forgot from "./Components/pages/Forgot";
+import Reset from "./Components/pages/Reset";
+import { Container, Row, Col } from "react-bootstrap";
 import Sidebar from "./Components/pages/Sidebar";
-import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
     <div className="App">
       <Router>
-        <MainNav /> 
-        <Container fluid className="">
+        {authenticated && <MainNav />}
+
+        <Container fluid>
           <Row>
-          <Col className="" lg={"2"} xxl={"2"} xl={"2"}>
-          <Sidebar/>
-          </Col>
-          <Col lg={"9"} xxl={"10"} xl={"10"} md={"12"} sm={"12"}>
-        <Routes>
-          <Route path="/" element={<AdminDashboard />} />
-          <Route path="/designation" element={<Designation/>} />
-          <Route path="/employee" element={<EmployeeDashboard/>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot" element={<Forgot />} />
-          <Route path="/reset" element={<Reset />} />
-        </Routes>
-          </Col>
+            {authenticated && (
+              <Col lg={2} xxl={2} xl={2}>
+              <Sidebar/>
+              </Col>
+            )}
+
+            <Col lg={10} xxl={10} xl={10} md={12} sm={12}>
+              <Routes>
+                {/* Public Routes */}
+                <Route path="/login" element={<Login setAuthenticated={setAuthenticated} />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/forgot" element={<Forgot />} />
+                <Route path="/reset" element={<Reset />} />
+
+                {/* Private Routes */}
+                <Route
+                  path="/"
+                  element={authenticated ? <AdminDashboard /> : <Navigate to="/login" />}
+                />
+                <Route
+                  path="/designation"
+                  element={authenticated ? <Designation /> : <Navigate to="/login" />}
+                />
+                <Route
+                  path="/employee"
+                  element={authenticated ? <EmployeeDashboard /> : <Navigate to="/login" />}
+                />
+              </Routes>
+            </Col>
           </Row>
         </Container>
       </Router>
